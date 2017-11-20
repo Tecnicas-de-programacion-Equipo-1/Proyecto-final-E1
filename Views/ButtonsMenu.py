ba5acc3b51cf77c19c5e8f21e41e611e4c380bd7
MaFer <fernandahonojosa502@gmail.com>
2017-11-19 20:48:33 -0600

    added rooms button action

--- Views/ButtonsMenu.py
+++ Views/ButtonsMenu.py
@@ -21,48 +21,41 @@ class Buttons():
         notifications_text = "Notificaciones"
         weather_text = "Informacion del Clima"
 
+    class Event:
+        click = "<Button-1>"
 
 
-    def __init__(self, master_rooms):
+    def __init__(self, master_rooms, action = None):
+        self.__action = action
         self.__rooms = master_rooms
         self.__notifications_button = Button(font=self.Constants.font)
         self.__fan_button = Button(font=self.Constants.font)
         self.__door_button = Button(font=self.Constants.font)
         self.__weather_label = Label(bg=self.Constants.weather_color, width=35, height=10, font=self.Constants.font)
+        self.__create_plane()
         self.__create_buttons()
+        self.__door_button.bind(self.Event.click, self.__did_tap)
 
-    def __create_buttons(self):
+    def __create_plane(self):
         self.__rooms.create_polygon((0, 0), (0, 200), (125, 200), (125, 175), (150, 175), (150, 0),
                                     fill=self.Constants.room_color, outline=self.Constants.black)
-        self.__rooms.create_text(75,100, font = self.Constants.font, text = self.Constants.bed_room_text)
-
         self.__rooms.create_polygon((0, 200), (0, 275), (125, 275), (125, 200), fill=self.Constants.room_color,
                                     outline=self.Constants.black)
-        self.__rooms.create_text(62.5, 237.5, font=self.Constants.font, text=self.Constants.bath_room_text)
-
         self.__rooms.create_polygon((50, 275), (50, 350), (125, 350), (125, 275), fill=self.Constants.room_color,
                                     outline=self.Constants.black)
-        self.__rooms.create_text(87.5, 312.5, font=self.Constants.font, text=self.Constants.bath_room_text)
-
         self.__rooms.create_polygon((0, 275), (50, 275), (50, 350), (100, 350), (100, 375), (150, 375), (150, 500),
                                     (0, 500), fill=self.Constants.room_color, outline=self.Constants.black)
-        self.__rooms.create_text(75, 437.5, font=self.Constants.font, text=self.Constants.garden_text)
-
         self.__rooms.create_polygon((150, 0), (150, 175), (300, 175), (300, 0), fill=self.Constants.room_color,
                                     outline=self.Constants.black)
-        self.__rooms.create_text(225, 87.5, font=self.Constants.font, text=self.Constants.kitchen_text)
-
         self.__rooms.create_polygon((125, 175), (125, 350), (100, 350), (100, 375), (300, 375), (300, 175),
                                     fill=self.Constants.room_color, outline=self.Constants.black)
-        self.__rooms.create_text(212.5, 275, font=self.Constants.font, text=self.Constants.living_room_text)
-
         self.__rooms.create_polygon((150, 375), (150, 500), (400, 500), (400, 375), fill=self.Constants.room_color,
                                     outline=self.Constants.black)
-        self.__rooms.create_text(275, 437.5, font=self.Constants.font, text=self.Constants.parking_text)
-
         self.__rooms.create_polygon((300, 0), (300, 200), (400, 200), (400, 0), fill=self.Constants.room_color,
                                     outline=self.Constants.black)
-        self.__rooms.create_text(350, 100, font=self.Constants.font, text=self.Constants.service_room_text)
+    
+    def __create_buttons(self):
+
 
         self.__weather_label.place(x=450, y=50)
         self.__weather_label.config(text=self.Constants.weather_text)
@@ -75,3 +68,7 @@ class Buttons():
 
         self.__door_button.configure(text = self.Constants.door_text, bg = self.Constants.red)
         self.__door_button.place(x = 525, y = 425)
+
+    def __did_tap(self, event):
+        if self.__action is None: return
+        self.__action("estacionamiento")
