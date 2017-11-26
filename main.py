2ec571cc4253494a6c4bfcbe6d9adc291b71e44a
RicardoSaid <said_s027@hotmail.es>
2017-11-26 14:20:17 -0600

    New branch of motion sensor

--- main.py
+++ main.py
@@ -7,24 +7,23 @@ class MainApp():
     def __init__(self):
         for port in list_ports.comports(include_links=True):
             print(port.device, port.name, port.description)
-
         self.__weather = WeatherManager.get_weather_data()
         self.__weather_text  = self.__weather.information
         self.__master = MainView(action = self.__on_off, weather_text = self.__weather_text)
-        self.__arduino = serial.Serial('COM8', 115200)
+        self.__arduino = serial.Serial('COM7', 115200)
         self.__master.protocol("WM_DELETE_WINDOW", self.__on_closing)
 
     def run(self):
         self.__master.mainloop()
 
-    def __on_off(self, sender, pin, state):
-        pin = str(pin).encode('ascii')
-        self.__arduino.write(pin)
+    def __on_off(self, sender, code, state):
+        data = ("1" + code) if state else ("0" + code)
+        data = str(data).encode('ascii')
+        self.__arduino.write(data)
 
     def __on_closing(self):
         self.__arduino.close()
         self.__master.destroy()
-
 
 if __name__ == "__main__":
     app = MainApp()
