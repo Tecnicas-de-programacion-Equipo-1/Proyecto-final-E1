f9c264415e735cc5319666ab7d17b7520ab6943f
RicardoSaid <said_s027@hotmail.es>
2017-11-26 14:31:05 -0600

    I added the funtion update clock and data handler for read the dates of motion sensor

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ class MainApp():
         self.__weather = WeatherManager.get_weather_data()
         self.__weather_text  = self.__weather.information
         self.__master = MainView(action = self.__on_off, weather_text = self.__weather_text, action_parking = self.__on__off_parking)
-        self.__arduino = serial.Serial('COM7', 115200)
+        self.__arduino = serial.Serial('COM6', 115200)
         self.__master.protocol("WM_DELETE_WINDOW", self.__on_closing)
+        self.__update_clock()
 
     def run(self):
         self.__master.mainloop()
@@ -24,6 +25,16 @@ class MainApp():
     def __on__off_parking(self, on_off, status):
         data = str(on_off).encode("ascii")
         self.__arduino.write(data)
+
+    def __handle_data(self, data):
+        clean_values = data.strip(' \n\r').split(",")
+        value = clean_values[0]
+        print(value)
+
+    def __update_clock(self):
+        data = self.__arduino.readline().decode()
+        self.__handle_data(data)
+        self.__master.after(1, self.__update_clock)
 
     def __on_closing(self):
         self.__arduino.close()
