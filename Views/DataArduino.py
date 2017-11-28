dc5494bc8563be1c7fb83cafb7bd8b53a8f00ce1
RicardoSaid <said_s027@hotmail.es>
2017-11-28 09:51:21 -0600

    I added the separation of responsabilities

---
+++ Views/DataArduino.py
@@ -0,0 +1,27 @@
+import serial
+from serial.tools import list_ports
+from Views.MainView import MainView
+
+class DataArduino():
+    def __init__(self):
+        self.__arduino = None
+
+    def read_port(self):
+        for port in list_ports.comports(include_links=True):
+            print(port.device, port.name, port.description)
+
+    def Arduino(self):
+        self.__arduino = serial.Serial('COM7', 115200)
+
+    def on_off(self, sender, code, state):
+        data = ("1" + code) if state else ("0" + code)
+        data = str(data).encode('ascii')
+        self.__arduino.write(data)
+
+    def on__off_parking(self, on_off, status):
+        data = str(on_off).encode("ascii")
+        self.__arduino.write(data)
+
+    def on_closing(self, master):
+        self.__arduino.close()
+        master.destroy()
--- main.py
+++ main.py
@@ -1,44 +1,27 @@
 from Views.MainView import MainView
 from Models.WeatherManager import WeatherManager
-import serial
-from serial.tools import list_ports
+from Views.DataArduino import DataArduino
 
 class MainApp():
     def __init__(self):
-        for port in list_ports.comports(include_links=True):
-            print(port.device, port.name, port.description)
+        DataArduino.read_port(self)
         self.__weather = WeatherManager.get_weather_data()
         self.__weather_text  = self.__weather.information
         self.__master = MainView(action = self.__on_off, weather_text = self.__weather_text, action_parking = self.__on__off_parking)
-        self.__arduino = serial.Serial('COM6', 115200)
+        DataArduino.Arduino(self)
         self.__master.protocol("WM_DELETE_WINDOW", self.__on_closing)
-        self.__update_clock()
 
     def run(self):
         self.__master.mainloop()
 
     def __on_off(self, sender, code, state):
-        data = ("1" + code) if state else ("0" + code)
-        data = str(data).encode('ascii')
-        self.__arduino.write(data)
+        DataArduino.on_off(self,sender,code,state)
 
     def __on__off_parking(self, on_off, status):
-        data = str(on_off).encode("ascii")
-        self.__arduino.write(data)
-
-    def __handle_data(self, data):
-        clean_values = data.strip(' \n\r').split(",")
-        value = clean_values[0]
-        self.__master.catch_values_sensor(value)
-
-    def __update_clock(self):
-        data = self.__arduino.readline().decode()
-        self.__handle_data(data)
-        self.__master.after(1, self.__update_clock)
+        DataArduino.on__off_parking(self,on_off,status)
 
     def __on_closing(self):
-        self.__arduino.close()
-        self.__master.destroy()
+        DataArduino.on_closing(self, self.__master)
 
 if __name__ == "__main__":
     app = MainApp()
